d88fd4f9d8595e5ff8435eb8c435eab6055afa8a
sans-connaissance <davemalicke@gmail.com>
2022-06-06 12:04:38 -0400

    ADD second test and funcs for catching negative number input error

--- TipCalculator/Models/TipCalculator.swift
+++ TipCalculator/Models/TipCalculator.swift
@@ -7,13 +7,23 @@
 
 import Foundation
 
+enum TipCalculatorError: Error {
+    case invalidInput
+}
 
 class TipCalculator {
     
-    func calculate(total: Double, tipPercentage: Double) -> Double {
+    func calculate(total: Double, tipPercentage: Double) throws -> Double {
+       
+        
+        if total < 0 {
+            throw TipCalculatorError.invalidInput
+        }
+        
         let tip = total * tipPercentage
         return tip
         
     }
     
 }
+
--- TipCalculatorUnitTests/TipCalculatorUnitTests.swift
+++ TipCalculatorUnitTests/TipCalculatorUnitTests.swift
@@ -11,8 +11,21 @@ class When_Calculating_Tip_Based_On_Total_Amount: XCTestCase {
 
     func test_should_calculate_tip_successfully() {
         let tipCalculator = TipCalculator()
-        let tip = tipCalculator.calculate(total:100, tipPercentage: 0.1)
+        let tip = try! tipCalculator.calculate(total:100, tipPercentage: 0.1)
         XCTAssertEqual(10, tip)
     }
 
+}
+
+class When_Calculating_Tip_Based_On_Negative_Total_Amount: XCTestCase {
+ 
+    func test_should_throw_invalid_input_exception() {
+        let tipCalculator = TipCalculator()
+        
+        XCTAssertThrowsError(try tipCalculator.calculate(total: -100, tipPercentage: 0.1), "") { error in
+            XCTAssertEqual(error as! TipCalculatorError, TipCalculatorError.invalidInput)
+            
+        }
+    }
+    
 }
