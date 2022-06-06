fc3bd63064c23821393b9dd98bdeea452767a261
sans-connaissance <davemalicke@gmail.com>
2022-06-06 10:57:40 -0400

    ADD basic logic test for calculating tip

---
+++ TipCalculator/Models/TipCalculator.swift
@@ -0,0 +1,19 @@
+//
+//  TipCalculator.swift
+//  TipCalculator
+//
+//  Created by David Malicke on 6/6/22.
+//
+
+import Foundation
+
+
+class TipCalculator {
+    
+    func calculate(total: Double, tipPercentage: Double) -> Double {
+        let tip = total * tipPercentage
+        return tip
+        
+    }
+    
+}
