6d793a99cf5d996e9836acebb7fc1905d10d7874
sans-connaissance <davemalicke@gmail.com>
2022-06-08 09:30:55 -0400

    REFACTOR UITests

--- TipCalculatorUITests/TipCalculatorUITests.swift
+++ TipCalculatorUITests/TipCalculatorUITests.swift
@@ -9,11 +9,16 @@ import XCTest
 
 class When_Content_View_Is_Shown: XCTestCase {
     
-    func test_should_make_sure_that_total_text_field_contains_default_value() {
-        
-        let app = XCUIApplication()
+    private var app: XCUIApplication!
+    
+    //run for each app
+    override func setUp() {
+        app = XCUIApplication()
         continueAfterFailure = false
         app.launch()
+    }
+    
+    func test_should_make_sure_that_total_text_field_contains_default_value() {
         
         let totalTextField = app.textFields["totalTextField"]
         XCTAssertEqual(totalTextField.value as! String, "Enter total")
@@ -22,9 +27,6 @@ class When_Content_View_Is_Shown: XCTestCase {
     
     func test_should_make_sure_the_20_percent_default_tip_option_is_selected() {
         
-        let app = XCUIApplication()
-        continueAfterFailure = false
-        app.launch()
         
         let picker = app.segmentedControls["selectedTipAmount"]
         let pickerButton = picker.buttons.element(boundBy: 1)
@@ -32,4 +34,10 @@ class When_Content_View_Is_Shown: XCTestCase {
         XCTAssertEqual(pickerButton.label, "20%")
         XCTAssertTrue(pickerButton.isSelected)
     }
+    
+    override func tearDown() {
+        //delete test files here if something is saved.
+        //clean up
+    }
+    
 }
