6245873b4fc7cde8b5dddf3d01fba64214be55eb
sans-connaissance <davemalicke@gmail.com>
2022-06-08 08:42:57 -0400

    ADD UITesting suite

---
+++ TipCalculatorUITests/TipCalculatorUITests.swift
@@ -0,0 +1,19 @@
+//
+//  TipCalculatorUITests.swift
+//  TipCalculatorUITests
+//
+//  Created by David Malicke on 6/8/22.
+//
+
+import XCTest
+
+class When_Content_View_Is_Shown: XCTestCase {
+    
+    func test_should_make_sure_that_total_text_field_contains_default_value() {
+        
+        let app = XCUIApplication()
+        continueAfterFailure = false
+        app.launch()
+        
+    }
+}
